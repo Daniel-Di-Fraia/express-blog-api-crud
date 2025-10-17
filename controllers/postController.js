a8ac206b7bb9c013c54d3b15c813581b50ece41a
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2025-10-17 13:32:55 +0200

    finalizzazione milestone 2 (logiche alle CRUD index, show e destroy)

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,12 +1,29 @@
 // importiamo i dati del blog
-const menu = require('../data/postsArray');
+const blogPosts = require('../data/postsArray');
 
 function index(req, res) {
-    res.send('Lista dei post');
+    res.json(blogPosts);
 }
 
 function show(req, res) {
-    res.send('Dettagli del post ' + req.params.id);
+    // recuperiamo l'id dall' URL e convertiamolo in un numero
+    const id = parseInt(req.params.id)
+
+    const post = blogPosts.find(post => post.id === id);
+
+    //controllo
+    if (!post) {
+
+        //status 404
+        res.status(404)
+
+        return res.json({
+            error: "Not Found",
+            message: "Post non trovato"
+        })
+    }
+    res.json(post);
+
 }
 
 function store(req, res) {
@@ -22,7 +39,31 @@ function modify(req, res) {
 }
 
 function destroy(req, res) {
-    res.send('Eliminazione del post ' + req.params.id);
+    // recuperiamo l'id
+    const id = parseInt(req.params.id)
+
+    // cerchiamo il post tramite id
+    const post = blogPosts.find(post => post.id === id);
+
+    //controllo
+    if (!post) {
+        res.status(404);
+
+        return res.json({
+            status: 404,
+            error: "Not Found",
+            message: "Post non trovato"
+        })
+    }
+
+    // Rimuoviamo il post dal blog
+    blogPosts.splice(blogPosts.indexOf(post), 1);
+
+    //controllo in log in terminale
+    console.log(blogPosts);
+
+    // Restituiamo lo status per l eliminazione andata a buon fine
+    res.sendStatus(204)
 }
 
 // esportiamo
